bb7fa2f57f47ea34225c27d212be9e7865ffde40
Bobby Steckline <rjsturing@gmail.com>
2023-10-12 22:24:18 -0400

    Add last working test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -10,7 +10,7 @@ describe("deck", () => {
     const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = createDeck([card1, card2, card3])
+     deck = createDeck([card1, card2, card3])
 })
 
 it("should be a function", () => {
